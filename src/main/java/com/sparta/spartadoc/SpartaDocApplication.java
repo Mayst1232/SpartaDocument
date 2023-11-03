589ec15549ca4382b11e5f9867c9ea7232054a55
hwang <ghkdrbwjd369@naver.com>
2023-11-03 20:10:00 +0900

    init

---
+++ src/main/java/com/sparta/spartadoc/SpartaDocApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spartadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaDocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaDocApplication.class, args);
+	}
+
+}
